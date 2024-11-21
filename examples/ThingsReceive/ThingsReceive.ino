0cdbcdf2571614491aaad1e42ca32a43b3ed24da
Consentium IoT <84334208+ConsentiumIoT@users.noreply.github.com>
2024-11-21 15:32:38 +0530

    Update ThingsReceive.ino

--- examples/ThingsReceive/ThingsReceive.ino
+++ examples/ThingsReceive/ThingsReceive.ino
@@ -7,7 +7,7 @@
   
   Connect a LED to GPIO 16 for ESP 8266, GPIO 23 for ESP 32 and GPIO 25 for Raspberry Pi Pico W to indicate the REST events. 
   
-  Written by Debjyoti Chowdhury for Consentium IoT.
+  Write to us at official@consentiumiot.com.
   MIT license, all text above must be included in any redistribution
  ****************************************************/
 

6f02cc18c2ec011ed4ec7589be52bb45ba584d75
Consentium IoT <84334208+ConsentiumIoT@users.noreply.github.com>
2024-12-05 14:56:59 +0530

    website path
    Moved to consentiumiot.com from consentiuminc.online.

--- src/utils/ConsentiumEssentials.h
+++ src/utils/ConsentiumEssentials.h
@@ -1,8 +1,7 @@
-
 // Sensor data update and receive URLs
-const char* send_url = "https://consentiuminc.online/api/board/updatedata/?";
-const char* receive_url = "https://consentiuminc.online/api/board/getdata/recent?";
+const char* send_url = "https://consentiumiot.com/api/board/updatedata/?";
+const char* receive_url = "https://consentiumiot.com/api/board/getdata/recent?";
 
 // Board OTA essential URLs
-const char* versionURL = "https://consentiuminc.online/api/board/firmware/version?";
-const char* firmwareURL = "https://consentiuminc.online/api/board/firmware/bin?";
+const char* versionURL = "https://consentiumiot.com/api/board/firmware/version?";
+const char* firmwareURL = "https://consentiumiot.com/api/board/firmware/bin?";
